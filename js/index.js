1c8244ab0a5812de6d2c691cd55e05c7f2f76c40
zaimsyaddad99 <zaimibnusyam@gmail.com>
2021-03-24 20:11:57 +0700

    add Cube Hollow file

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import initShader from './loader/shaders.js';
 import GLObject from './objects/GLObject.js';
+import { cubeGenerator } from './objects/Cube.js';
 import { makePyramidEdges } from './objects/Pyramid.js';
 import Renderer from './renderer.js';
 import { fGenerator, blockGenerator } from './utils/generator.js';
@@ -15,7 +16,7 @@ var objId = 0;
 /* Setting max translation of each x, y, z */
 document.getElementById('transX').max = canvas.width;
 document.getElementById('transY').max = canvas.height;
-document.getElementById('transZ').max = (canvas.width + canvas.height)/2;
+document.getElementById('transZ').max = (canvas.width + canvas.height) / 2;
 
 /* For translation X */
 var transXSlider = document.getElementById("transX");
@@ -24,7 +25,7 @@ transXVal.innerHTML = transXSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 transXSlider.oninput = function() {
-  transXVal.innerHTML = this.value;
+    transXVal.innerHTML = this.value;
 }
 
 /* For translation Y */
@@ -34,7 +35,7 @@ transYVal.innerHTML = transYSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 transYSlider.oninput = function() {
-  transYVal.innerHTML = this.value;
+    transYVal.innerHTML = this.value;
 }
 
 /* For translation Z */
@@ -44,7 +45,7 @@ transZVal.innerHTML = transZSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 transZSlider.oninput = function() {
-  transZVal.innerHTML = this.value;
+    transZVal.innerHTML = this.value;
 }
 
 /* For rotation X */
@@ -54,7 +55,7 @@ rotXVal.innerHTML = rotXSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 rotXSlider.oninput = function() {
-  rotXVal.innerHTML = this.value;
+    rotXVal.innerHTML = this.value;
 }
 
 /* For rotation Y */
@@ -64,7 +65,7 @@ rotYVal.innerHTML = rotYSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 rotYSlider.oninput = function() {
-  rotYVal.innerHTML = this.value;
+    rotYVal.innerHTML = this.value;
 }
 
 /* For rotation Z */
@@ -74,7 +75,7 @@ rotZVal.innerHTML = rotZSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 rotZSlider.oninput = function() {
-  rotZVal.innerHTML = this.value;
+    rotZVal.innerHTML = this.value;
 }
 
 /* For scaling X */
@@ -84,7 +85,7 @@ scaXVal.innerHTML = scaXSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 scaXSlider.oninput = function() {
-  scaXVal.innerHTML = this.value;
+    scaXVal.innerHTML = this.value;
 }
 
 /* For scaling Y */
@@ -94,7 +95,7 @@ scaYVal.innerHTML = scaYSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 scaYSlider.oninput = function() {
-  scaYVal.innerHTML = this.value;
+    scaYVal.innerHTML = this.value;
 }
 
 /* For sacling Z */
@@ -104,7 +105,7 @@ scaZVal.innerHTML = scaZSlider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 scaZSlider.oninput = function() {
-  scaZVal.innerHTML = this.value;
+    scaZVal.innerHTML = this.value;
 }
 
 async function main() {
@@ -127,34 +128,32 @@ async function main() {
     let top = 0;
     let near = 400;
     let far = -400;
-    let aspect = right/bottom;
-    
+    let aspect = right / bottom;
+
     /* Projection */
     var proj = document.getElementById('proj');
     /* For fov*/
     var fovSlider = document.getElementById("fov");
     var fovVal = document.getElementById("fovVal");
     fovVal.innerHTML = fovSlider.value; // Display the default slider value
-    
+
     // Update the current slider value (each time you drag the slider handle)
     fovSlider.oninput = function() {
-      fovVal.innerHTML = this.value;
+        fovVal.innerHTML = this.value;
     }
-    
+
     var fov = parseInt(fovSlider.value);
     console.log(fov);
     let baseProjection;
     if (proj.value === '0') {
-      baseProjection = orthographic(left, right, bottom, top, near, far);
+        baseProjection = orthographic(left, right, bottom, top, near, far);
+    } else if (proj.value === '1') {
+        baseProjection = perspective(fov, aspect, near, far);
+    } else if (proj.value === '2') {
+        baseProjection = orthographic(left, right, bottom, top, near, far);
     }
-    else if (proj.value === '1') {
-      baseProjection = perspective(fov, aspect, near, far);
-    }
-    else if (proj.value === '2') {
-      baseProjection = orthographic(left, right, bottom, top, near, far);
-    }
-    
-    
+
+
     // Declare reusable variables
     let vertices, colors;
 
@@ -167,15 +166,9 @@ async function main() {
     glObject.setRotation(40, 25, 325);
     glObject.setScale(1, 1, 1);
     glObject.bind();
-    
+
     [vertices, colors] = blockGenerator(
-        50, 50, 50,
-        [200, 70, 120],
-        [80, 70, 200],
-        [255, 0, 0],
-        [70, 200, 210],
-        [200, 200, 70],
-        [160, 160, 220]
+        50, 50, 50, [200, 70, 120], [80, 70, 200], [255, 0, 0], [70, 200, 210], [200, 200, 70], [160, 160, 220]
     );
     const glObject2 = new GLObject(0, program, gl, gl.TRIANGLES);
     glObject2.setBaseProjectionMatrix(baseProjection);
@@ -185,15 +178,9 @@ async function main() {
     glObject2.setRotation(90, 0, 0);
     glObject2.setScale(1, 1, 1);
     glObject2.bind();
-    
+
     [vertices, colors] = blockGenerator(
-        50, 50, 300,
-        [200, 70, 120],
-        [80, 70, 200],
-        [255, 0, 0],
-        [70, 200, 210],
-        [200, 200, 70],
-        [160, 160, 220]
+        50, 50, 300, [200, 70, 120], [80, 70, 200], [255, 0, 0], [70, 200, 210], [200, 200, 70], [160, 160, 220]
     );
     const cube = new GLObject(0, program, gl, gl.TRIANGLES);
     cube.setBaseProjectionMatrix(baseProjection);
@@ -220,21 +207,27 @@ async function main() {
         opt.text = i.id;
         opt.value = i.id;
         select.add(opt);
-    }) 
+    })
 
-    
+
     // var diffXTrans, diffYTrans, diffZTrans,
     // diffXRot, diffYRot, diffZRot,
     // diffXSca, diffYSca, diffZSca
     // var object = renderer.objects;
-    let xTrans = [],yTrans = [],zTrans = [],
-    xRot = [], yRot = [], zRot = [],
-    xScale = [], yScale = [], zScale = [];
-    
+    let xTrans = [],
+        yTrans = [],
+        zTrans = [],
+        xRot = [],
+        yRot = [],
+        zRot = [],
+        xScale = [],
+        yScale = [],
+        zScale = [];
+
     renderer.objects.forEach(obj => {
         let tempXTrans, tempYTrans, tempZTrans,
-        tempXRot, tempYRot, tempZRot,
-        tempXScale,tempYScale, tempZScale;
+            tempXRot, tempYRot, tempZRot,
+            tempXScale, tempYScale, tempZScale;
         [tempXTrans, tempYTrans, tempZTrans] = obj.getPosition();
         [tempXRot, tempYRot, tempZRot] = obj.getRotation();
         [tempXScale, tempYScale, tempZScale] = obj.getScale();
@@ -255,24 +248,22 @@ async function main() {
         let obj = renderer.objects[selectedObjId];
         fov = parseInt(fovSlider.value);
         renderer.objects.forEach(o => {
-          if (proj.value === '0') {
-            o.setBaseProjectionMatrix(orthographic(left, right, bottom, top, near, far));
-            // console.log(proj.value);
-          }
-          else if (proj.value === '1') {
-            o.setBaseProjectionMatrix(perspective(fov, aspect, near, far));
-            // console.log(proj.value);
-            
-          }
-          else if (proj.value === '2') {
-            o.setBaseProjectionMatrix(orthographic(left, right, bottom, top, near, far));
-            // console.log(proj.value);
-            
-            
-          }
-  
+            if (proj.value === '0') {
+                o.setBaseProjectionMatrix(orthographic(left, right, bottom, top, near, far));
+                // console.log(proj.value);
+            } else if (proj.value === '1') {
+                o.setBaseProjectionMatrix(perspective(fov, aspect, near, far));
+                // console.log(proj.value);
+
+            } else if (proj.value === '2') {
+                o.setBaseProjectionMatrix(orthographic(left, right, bottom, top, near, far));
+                // console.log(proj.value);
+
+
+            }
+
         })
-        
+
         // console.log(selectedObj);
         gl.clearColor(1, 1, 1, 1);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -287,13 +278,13 @@ async function main() {
         // cube.setRotation(x + 0.1, y + 1 , z);
         // [x,y,z] = pyramid.getRotation();
         // pyramid.setRotation(x+0.5,y+0.5,z+0.5);
-        
-        obj.setPosition(xTrans[selectedObjId]+parseFloat(transXSlider.value), yTrans[selectedObjId]+parseFloat(transYSlider.value), zTrans[selectedObjId]+parseFloat(transZSlider.value));
+
+        obj.setPosition(xTrans[selectedObjId] + parseFloat(transXSlider.value), yTrans[selectedObjId] + parseFloat(transYSlider.value), zTrans[selectedObjId] + parseFloat(transZSlider.value));
         obj.setRotation(parseFloat(rotXSlider.value), parseFloat(rotYSlider.value), parseFloat(rotZSlider.value));
         obj.setScale(parseFloat(scaXSlider.value), parseFloat(scaYSlider.value), parseFloat(scaZSlider.value));
-          
+
         renderer.render();
-        
+
         requestAnimationFrame(render);
     }
     requestAnimationFrame(render);
